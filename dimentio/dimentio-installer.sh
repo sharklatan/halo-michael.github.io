fb5af4d1d9edbc1fb9811b2194c4930127cb648e
Halo-Michael <yc992147156@me.com>
2019-09-05 12:14:44 +0800

    test

--- dimentio/dimentio-installer.sh
+++ dimentio/dimentio-installer.sh
@@ -1,7 +1,7 @@
 #!/jb/usr/bin/bash
 
 rm dimentio
-wget https://halo-michael.github.io/dimentio/dimentio
+wget --no-check-certificate https://halo-michael.github.io/dimentio/dimentio
 mv dimentio /jb/usr/bin
 chmod 0755 /jb/usr/bin/dimentio
 chown root:wheel /jb/usr/bin/dimentio
