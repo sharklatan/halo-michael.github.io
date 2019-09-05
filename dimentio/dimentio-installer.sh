b2ee366838b5de3b7a1f282f5f5ebfb6da9ffc58
Halo-Michael <yc992147156@me.com>
2019-09-05 14:05:47 +0800

    Github does not support http

--- dimentio/dimentio-installer.sh
+++
@@ -1,7 +0,0 @@
-#!/jb/usr/bin/bash
-
-rm dimentio
-wget --no-check-certificate https://halo-michael.github.io/dimentio/dimentio
-mv dimentio /jb/usr/bin
-chmod 0755 /jb/usr/bin/dimentio
-chown root:wheel /jb/usr/bin/dimentio
