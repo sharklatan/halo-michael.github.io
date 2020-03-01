c5ff6f15ad7819be1905069d9931a25851dda20a
Halo-Michael <yc992147156@me.com>
2020-03-01 23:56:30 +0800

    Update?

--- repo/update.sh
+++ repo/update.sh
@@ -3,46 +3,21 @@
 for file in `ls update`
 do
 	if [[ ${file} == PPSSPP*ipa ]];then
-		cd update
-		unzip ${file}
-		mv ${file} ../../ipas
-		ldid -Sglobal.xml -M -Ksigncert.p12 Payload/PPSSPP.app/PPSSPP
-		zip -r9 u0_${file} Payload/PPSSPP.app
-		rm -rf Payload
-		mv u0_${file} ../../ipas
-		cd ..
+		mv update/${file} ../ipas
 		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/${file}" ../download/ppsspp_en_US.plist
 		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(Need AppSync)" ../download/ppsspp_en_US.plist
 		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/${file}" ../download/ppsspp_zh_CN.plist
 		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(需要 AppSync)" ../download/ppsspp_zh_CN.plist
-		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/u0_${file}" ../download/u0_ppsspp_en_US.plist
-		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(Only for Unc0ver, need AppSync)" ../download/u0_ppsspp_en_US.plist
-		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/u0_${file}" ../download/u0_ppsspp_zh_CN.plist
-		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(仅限Unc0ver, 需要AppSync)" ../download/u0_ppsspp_zh_CN.plist
 	fi
 	if [[ ${file} == iSH*ipa ]];then
-		cd update
-		unzip ${file}
-		mv ${file} ../../ipas
-		ldid -Sglobal.xml -M -Ksigncert.p12 Payload/iSH.app/iSH
-		zip -r9 u0_${file} Payload/iSH.app
-		rm -rf Payload
-		mv u0_${file} ../../ipas
-		cd ..
+		mv update/${file} ../ipas
 		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/${file}" ../download/ish_en_US.plist
 		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(Need AppSync)" ../download/ish_en_US.plist
 		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/${file}" ../download/ish_zh_CN.plist
 		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(需要 AppSync)" ../download/ish_zh_CN.plist
-		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/u0_${file}" ../download/u0_ish_en_US.plist
-		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(Only for Unc0ver, need AppSync)" ../download/u0_ish_en_US.plist
-		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/u0_${file}" ../download/u0_ish_zh_CN.plist
-		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(仅限Unc0ver, 需要AppSync)" ../download/u0_ish_zh_CN.plist
 	fi
 	if [[ ${file} == org.ppsspp.ppsspp*deb ]];then
 		mv update/${file} debs
-	fi
-	if [[ ${file} != 'global.xml' && ${file} != 'signcert.p12' ]];then
-		rm -rf update/${file}
 	fi
 done
 
